2fef022fc22b549400d0a2daf85a0db71728a915
konp <konp88@gmail.com>
2017-10-27 15:21:28 +0800

    dgango blog.article

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^index', views.index, name='index'),
+    url(r'^home', views.home, name='home'),
 ]
--- blog/article/views.py
+++ blog/article/views.py
@@ -7,9 +7,33 @@ from django.db import models
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+import pymysql.cursors
 
+def search_db():
+    config = {'host': '127.0.0.1',
+              'port': 3306,
+              'user': 'root',
+              'password': 'root',
+              'db': 'test',
+              'cursorclass': pymysql.cursors.DictCursor,}
+    connection = pymysql.connect(**config)
+
+    try:
+        with connection.cursor() as cursor:
+            sql = 'SELECT * from employee1'
+            cursor.execute(sql)
+            result = cursor.fetchone()
+        connection.commit()
+
+    finally:
+        connection.close()
+    return result
+
+
+from django.template.loader import render_to_string
 def index(request):
-    return HttpResponse("hello world!!!")
+    list_info = search_db()
+    return render_to_response('home.html', {'employee': list_info})
 
-def hello(request):
-    pass
+def home(request):
+    return HttpResponse('hello world')
