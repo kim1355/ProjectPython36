f750d7b205ab160034a12254aa15846d35e78163
konp <konp88@gmail.com>
2017-11-11 00:54:57 +0800

    spider test

--- SpiderTest.py
+++ SpiderTest.py
@@ -18,23 +18,21 @@ from bs4 import BeautifulSoup
 
 res = requests.get('http://www.meituan.com/api/v1/divisions?mtt=1.help%2Fapi.0.0.j9u06211')
 res.encoding = 'utf-8'
-soup = BeautifulSoup(res.text,'xml')
+soup = BeautifulSoup(res.text, 'xml')
 jdata = soup.select('division')
 name = re.compile(r'<name>(.*?)</name>').findall(str(jdata))
 latitude = re.compile(r'<latitude>(.*?)</latitude>').findall(str(jdata))
 longitude = re.compile(r'<longitude>(.*?)</longitude>').findall(str(jdata))
 
+L5 = []
+for i in range(len(name)):
+    L4 = []
+    L4.append(name[i])
+    L4.append(latitude[i])
+    L4.append(longitude[i])
+    L5.append(L4)
 
-df1 = pandas.DataFrame(name)
-df2 = pandas.DataFrame(latitude)
-df3 = pandas.DataFrame(longitude)
-
-df1.columns = ['城市']
-df2.columns = ['纬度']
-df3.columns = ['经度']
-
-# frame = [df1, df2, df3]
-# df4 = pandas.concat(frame)
-df4 = df1.append([df2, df3])
-df4.to_excel('E://aa.xlsx')
-
+df = pandas.DataFrame(L5)
+df.columns = ['城市', '纬度', '经度']
+# print(df)
+df.to_excel('E://美团.xlsx')
