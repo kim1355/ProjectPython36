2f87c2f8ccba9eaceb9bc68197e123b8ec9dfd99
konp <konp88@gmail.com>
2017-11-11 01:03:20 +0800

    spider test

--- SpiderTest.py
+++ SpiderTest.py
@@ -18,21 +18,20 @@ from bs4 import BeautifulSoup
 
 res = requests.get('http://www.meituan.com/api/v1/divisions?mtt=1.help%2Fapi.0.0.j9u06211')
 res.encoding = 'utf-8'
+
 soup = BeautifulSoup(res.text, 'xml')
 jdata = soup.select('division')
+
 name = re.compile(r'<name>(.*?)</name>').findall(str(jdata))
 latitude = re.compile(r'<latitude>(.*?)</latitude>').findall(str(jdata))
 longitude = re.compile(r'<longitude>(.*?)</longitude>').findall(str(jdata))
 
-L5 = []
+L1, L2 = [], []
 for i in range(len(name)):
-    L4 = []
-    L4.append(name[i])
-    L4.append(latitude[i])
-    L4.append(longitude[i])
-    L5.append(L4)
+    L1 = [name[i], latitude[i], longitude[i]]  # 注意： L4 每遍历一次 需要重新赋值重置，不能append叠加
+    L2.append(L1)   # 赋值叠加
 
-df = pandas.DataFrame(L5)
+df = pandas.DataFrame(L2)
 df.columns = ['城市', '纬度', '经度']
 # print(df)
 df.to_excel('E://美团.xlsx')
