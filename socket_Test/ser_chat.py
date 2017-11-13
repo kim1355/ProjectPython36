803b8c32d51fcdf0c64667f635e0f46d316057a0
konp <konp88@gmail.com>
2017-11-13 13:01:11 +0800

    socket test

---
+++ socket_Test/ser_chat.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@version: python2.7
+@author: Administrator
+@license: iTech Licence 
+@contact: konp88@gmail.com
+@site: 
+@software: PyCharm
+@file: ser_chat.py
+@time: 2017/11/13 12:59
+"""
