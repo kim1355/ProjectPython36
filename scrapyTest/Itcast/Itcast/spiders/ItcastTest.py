758f1212d3456a9b6782de69b03db620b2cc45d3
konp <konp88@gmail.com>
2017-11-13 13:00:54 +0800

    socket test

--- scrapyTest/Itcast/Itcast/spiders/ItcastTest.py
+++ scrapyTest/Itcast/Itcast/spiders/ItcastTest.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from Itcast.items import ItcastItem
+
 
 
 class ItcasttestSpider(scrapy.Spider):
@@ -9,14 +11,20 @@ class ItcasttestSpider(scrapy.Spider):
 
     def parse(self, response):
         # print(response.body)    # 显示网页内容
-
+        item = ItcastItem()       # items.py中定义的数据模板
         node_list = response.xpath("//div[@class='li_txt']")
+
         for node in node_list:
             name = node.xpath('./h3/text()').extract()
             title = node.xpath('./h4/text()').extract()
             info = node.xpath('./p/text()').extract()
 
-            print(name)
-            print(title)
-            print(info)
+            item['name'] = name[0]
+            item['title'] = title[0]
+            item['info'] = info[0]
+            # print(name)
+            # print(title)
+            # print(info)
+            # 生成器，逐一返回给引擎，然后引擎返回给pipelines.py分析存储
+            yield item
         # pass
---
+++ socket_Test/cli_chat.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@version: python2.7
+@author: Administrator
+@license: iTech Licence 
+@contact: konp88@gmail.com
+@site: 
+@software: PyCharm
+@file: cli_chat.py
+@time: 2017/11/13 12:59
+"""
