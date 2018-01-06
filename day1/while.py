28fec94693d1d154cc14d495db46b8b204588a8c
konp <konp88@gmail.com>
2018-01-06 16:41:36 +0800

    hello

---
+++ day1/while.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@version: python2.7
+@author: Administrator
+@license: iTech Licence 
+@contact: konp88@gmail.com
+@site: 
+@software: PyCharm
+@file: while.py
+@time: 2017/12/16 21:40
+"""
