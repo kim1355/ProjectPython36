d9a4051d8a2f968bb1b5e5802c2f5e0c0121f63f
konp <konp88@gmail.com>
2019-05-14 15:43:26 +0800

    add getWK.py

---
+++ getWK.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@version: python3.6
+@author: ZengQi Wu
+@license: iTech Licence 
+@contact: konp88@gmail.com
+@site: 
+@software: PyCharm
+@file: getWK.py
+@time: 2018/6/13 10:08
+"""
